9e4ec32b95580c248bf2904f0641433de2a001a6
rahulsinghchauhanrj <rahulsinghchauhanrj@gmail.com>
2025-09-19 06:56:36 +0000

    Footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-secondary py-8 text-text-secondary">
       <div className="container mx-auto text-center">
-        <p>&copy; 2024 Ritik Vani. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Ritik Vani. All rights reserved.</p>
         <div className="mt-6 flex justify-center space-x-6">
           <a href="https://github.com/" className="text-text-secondary hover:text-accent transition-colors duration-300 transform hover:scale-110">
             <i className="fab fa-github fa-2x"></i>
